fed370960d3982bf547275e2facb52ca3c4c23e8
Ajgor13 <igorprz13@gmail.com>
2025-07-27 16:47:33 +0100

    Rename main class, make window resizable, and set window title

--- Code-Snippet-Search-Engine/src/main/java/com/igor/codesnippet/codesnippetsearchengine/MainApplication.java
+++ Code-Snippet-Search-Engine/src/main/java/com/igor/codesnippet/codesnippetsearchengine/MainApplication.java
@@ -2,18 +2,19 @@ package com.igor.codesnippet.codesnippetsearchengine;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
+    FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
+    Parent root = fxmlLoader.load();
+    Scene scene = new Scene(root);
+    stage.setTitle("Code Snippet Search Engine");
     stage.setScene(scene);
     stage.show();
   }
