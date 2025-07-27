f58f610be58e51e22ad1ab1a2fbffdd8c5b9e7b9
Ajgor13 <igorprz13@gmail.com>
2025-07-27 16:33:15 +0100

    Connect FileScanner and FilePathExplorer to UI

--- Code-Snippet-Search-Engine/src/main/java/com/igor/codesnippet/codesnippetsearchengine/MainController.java
+++ Code-Snippet-Search-Engine/src/main/java/com/igor/codesnippet/codesnippetsearchengine/MainController.java
@@ -1,17 +1,64 @@
 package com.igor.codesnippet.codesnippetsearchengine;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
-public class HelloController {
+public class MainController {
+  @FXML
+  private TextField fileExplorerField;
+  @FXML
+  private TextField keywordField;
+  @FXML
+  private TableView<TableRowEntry> resultTable;
+  @FXML
+  private TableColumn<TableRowEntry, String> fileColumn;
 
   @FXML
-  private Label welcomeText;
+  private TableColumn<TableRowEntry, Integer> countColumn;
 
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  public void initialize() {
+    fileColumn.setCellValueFactory(rowData -> new ReadOnlyStringWrapper(rowData.getValue().getFileName()));
+    countColumn.setCellValueFactory(rowData -> new ReadOnlyObjectWrapper<>(rowData.getValue().getKeywordCount()));
+  }
+  @FXML
+  protected void searchButtonClick() {
+    String filepath = fileExplorerField.getText();
+    String keyword = keywordField.getText();
+    if(keywordField.getText().isEmpty()) {
+      return;
+    }
 
+    HashMap<String, Integer> result = FileScanner.scanFiles(FilePathExplorer.getFileNames(filepath), keyword);
+    ObservableList<TableRowEntry> data = FXCollections.observableArrayList();
 
+    for (HashMap.Entry<String, Integer> entry : result.entrySet()) {
+      data.add(new TableRowEntry(entry.getKey(), entry.getValue()));
+    }
+
+    resultTable.setItems(data);
+  }
+  @FXML
+  protected void openSelectDirectory(){
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    directoryChooser.setTitle("Select Folder to Scan");
+
+    File selectedDirectory = directoryChooser.showDialog(null);
+
+    if (selectedDirectory != null) {
+      fileExplorerField.setText(selectedDirectory.getAbsolutePath());
+    }
   }
 }
